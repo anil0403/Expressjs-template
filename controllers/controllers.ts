6834261ffe9fb86f61286c5de708d7a48259f4b2
anil <anilshrestha0403.as@gmail.com>
2023-05-17 13:15:08 -0700

    controllers, routes created and api tested for posts

--- controllers/controllers.ts
+++ controllers/controllers.ts
@@ -22,7 +22,11 @@ export const createUser = async (req: Request, res: Response) => {
 };
 
 export const getUsers = async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
   res.status(200).json({ users });
   try {
   } catch (error) {
@@ -62,3 +66,79 @@ export const deleteUser = async (req: Request, res: Response) => {
 };
 
 // post controllers
+
+export const getPosts = async (req: Request, res: Response) => {
+  try {
+    const posts = await prisma.post.findMany();
+    res.status(200).json({ posts });
+  } catch (error) {
+    console.log(error);
+    res.status(200).json({ message: error.message });
+  }
+};
+
+export const getPostsByUserId = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const posts = await prisma.post.findMany({
+      where: {
+        userId: id,
+      },
+    });
+    res.status(200).json({ posts });
+  } catch (error) {
+    console.log(error);
+    res.status(200).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req: Request, res: Response) => {
+  const { title, description, userId } = req.body;
+  try {
+    const createdPost = await prisma.post.create({
+      data: {
+        title: title,
+        description: description,
+        userId: userId,
+      },
+    });
+    res.status(200).json({ createdPost });
+  } catch (error) {
+    console.log(error);
+    res.status(200).json({ message: error.message });
+  }
+};
+
+export const deletePost = async (req: Request, res: Response) => {
+  const { id } = req.body;
+  try {
+    const deletedPost = await prisma.post.delete({
+      where: {
+        id: id,
+      },
+    });
+    res.status(200).json({ deletedPost });
+  } catch (error) {
+    console.log(error);
+    res.status(200).json({ message: error.message });
+  }
+};
+
+export const updatePost = async (req: Request, res: Response) => {
+  const { id, title, description } = req.body;
+  try {
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: id,
+      },
+      data: {
+        title: title,
+        description: description,
+      },
+    });
+    res.status(200).json({ updatedPost });
+  } catch (error) {
+    console.log(error);
+    res.status(200).json({ message: error.message });
+  }
+};
--- routes/routes.ts
+++ routes/routes.ts
@@ -6,11 +6,23 @@ import {
   createUser,
   getUserById,
   deleteUser,
+  createPost,
+  getPosts,
+  getPostsByUserId,
+  deletePost,
+  updatePost,
 } from "../controllers/controllers";
 
+// users
 router.get("/get-users", getUsers);
 router.get("/get-user-by-id/:id", getUserById);
 router.post("/register", createUser);
 router.delete("/delete-user", deleteUser);
+
+// post
+router.get("/get-posts", getPosts);
+router.get("/get-by-user-id/:id", getPostsByUserId);
+router.post("/create-post", createPost);
+router.patch("/update-post", updatePost);
 
 export default router;

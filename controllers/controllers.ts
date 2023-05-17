93c1c64094c056a5738f70d1501127bceee20d90
anil <anilshrestha0403.as@gmail.com>
2023-05-17 11:44:12 -0700

    prisma model setup

--- controllers/controllers.ts
+++ controllers/controllers.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
 
+// user controllers
 export const getUsers = (req: Request, res: Response) => {
   res.status(200).json({ message: "Hello Express" });
 };
+
+
+// post controllers
